32ad836d12b4b8a18a8bcb851821352fbc48268f
Ozelot Vanilla <ozelotvanilla@gmail.com>
2025-10-23 18:45:32 +0900

    feature - Add code to also download `stop.sh`

--- install.sh
+++ install.sh
@@ -1,20 +1,26 @@
 # Used for Morimotolab.
 #
 # This script install SuperCollider and other required software.
-# It will create a `start.sh` and `start.scd` in `/home/pi`.
+# It will create a `start.sh`, `start.scd`, and `stop.sh` in `/home/pi`.
 # `start.sh` will launch SuperCollider and run `start.scd`.
+# `stop.sh` will stop all SuperCollider-related process.
 
 home_directory="/home/pi"
 
 start_sh_url="https://raw.githubusercontent.com/OzelotVanilla/RaspiSuperColliderInstaller/refs/heads/main/start.sh"
+stop_sh_url="https://raw.githubusercontent.com/OzelotVanilla/RaspiSuperColliderInstaller/refs/heads/main/stop.sh"
 
 cd $home_directory
 
+# Download necessary packages.
 sudo apt update -y
 sudo apt full-upgrade -y
 sudo apt install -y jackd2 # yes, for RT priority
 sudo apt install -y supercollider
 
+# Download two scripts.
 wget -P "$home_directory" -O start.sh "$start_sh_url"
+wget -P "$home_directory" -O stop.sh  "$stop_sh_url"
 
+# Create empty `start.scd`.
 touch "$home_directory/start.scd"
---
+++ stop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# sclang
+killall sclang
+
+# server
+killall scsynth
+killall supernova
+
+# jack
+killall jackd
